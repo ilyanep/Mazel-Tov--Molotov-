fc2e4c3ad249166f2eebb45ac86c8923e41268f9
Isaac Sheff <isheff@Owl.local>
2011-10-03 21:12:10 -0700

    hello world program running successfully . . . just getting back into c++ . . .

---
+++ binary_files/binary_files.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <math.h>
+using namespace std;
+
+
+int main (int argc, char **argv)
+{
+     cout << "hello, world! \n";
+     return 0;
+}
