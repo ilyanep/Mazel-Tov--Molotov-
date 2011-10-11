b491d9cfb9e83adad7f26c43d3bc46b2fda58b6d
Ilya Nepomnyashchiy <ilyanep@gmail.com>
2011-10-10 19:13:06 -0700

    added all_3_predictor

---
+++ all_3_predictor/all_3_predictor.cpp
@@ -0,0 +1,9 @@
+#include "all_3_predictor.h"
+#include "../learning_method.h"
+
+// The all 3 predictor will always return 3. This is more of an exercise in writing a predictor
+// than anything else. Also Yaser wants something by Tuesday. 
+    void All3Predictor::learn(int partition) { }
+    void All3Predictor::remember(int partition) { }
+
+    double All3Predictor::predict(int user, int movie, int time) { return 3.0; }
---
+++ all_3_predictor/standalone_all_3.cpp
@@ -0,0 +1,12 @@
+#include "all_3_predictor.h"
+#include "../write_data/write_results.h"
+
+int main() {
+    All3Predictor predictor;
+    predictor.learn(5);
+    vector<double> res;
+    for(int i=0; i < 2749898; ++i) {
+        res.push_back(predictor.predict(i,i,i));
+    }
+    output_results(res);
+}
