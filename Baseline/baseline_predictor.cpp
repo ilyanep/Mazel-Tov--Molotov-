4313a1490d921d164629762aea4bd8454a48cae5
Daniil Kitchaev <dkitch@caltech.edu>
2011-10-16 16:48:44 -0700

    Added baseline predictor. So far does global average, global user/movie bias, and some time effect fitting: movies are binned by "time bins" as described by BellKor and users have a linear regression fit for rating over time, where time is scaled by a power. The power and regularization parameter for the user time effect may need to be optimized some more.

--- Baseline/baseline_predictor.cpp
+++ Baseline/baseline_predictor.cpp
@@ -8,22 +8,27 @@ using namespace std;
 
 Baseline::Baseline(){
     //Initially all matrix elements are set to 0.0
-    userBias = gsl_matrix_calloc(USER_COUNT, 1); //Extra dimension for
-    movieBias = gsl_matrix_calloc(MOVIE_COUNT, 1); //computational stuff
+    userBias = gsl_matrix_calloc(USER_COUNT, 3); //Extra dimension for
+    movieBias = gsl_matrix_calloc(MOVIE_COUNT, NUM_MOVIE_BINS); //computational stuff
     data_loaded = false;
     load_data();
 }
 
 void Baseline::learn(int partition){
     assert(data_loaded);
+    printf("Finding global biases...\n");
     calculate_average_biases(partition);
-
+    printf("Finding movie time effects...\n");
+    calculate_movie_time_effects(partition);
+    printf("Finding user time effects...\n");
+    calculate_user_time_effects(partition);
 }
 
 void Baseline::calculate_average_biases(int partition){
     int user;
     int movie;
     float bias;
+    printf("\tCalculating movie biases...\n");
     gsl_matrix *userBias_t = gsl_matrix_calloc(USER_COUNT, 2);
     gsl_matrix *movieBias_t = gsl_matrix_calloc(MOVIE_COUNT, 2);
     for(int point = 0; point < DATA_COUNT; point++){
@@ -32,19 +37,21 @@ void Baseline::calculate_average_biases(int partition){
 
             gsl_matrix_set(movieBias_t, movie-1, 0,
                 gsl_matrix_get(movieBias_t, movie-1, 0) +
-                get_mu_all_rating(point) - AVG_RATING);
+                get_mu_all_rating(point) - (double)AVG_RATING);
 
             gsl_matrix_set(movieBias_t, movie-1, 1,
                 gsl_matrix_get(movieBias_t, movie-1, 1) + 1);
         }
     }
-
+    printf("\tSaving movie biases...\n");
     for(int movie = 0; movie < MOVIE_COUNT; movie++){
         bias = ((float)gsl_matrix_get(movieBias_t, movie, 0)) /
                ((float)REGUL_BIAS_MOVIE + gsl_matrix_get(movieBias_t, movie, 1));
-        gsl_matrix_set(movieBias, movie, 0, bias);
+        for(int bin = 0; bin < NUM_MOVIE_BINS; bin++){
+            gsl_matrix_set(movieBias, movie, bin, bias);
+        }
     }
-
+    printf("\tCalculating user biases...\n");
     for(int point = 0; point < DATA_COUNT; point++){
         if(get_mu_idx_ratingset(point) < partition){
             user = get_mu_all_usernumber(point);
@@ -52,14 +59,14 @@ void Baseline::calculate_average_biases(int partition){
 
             gsl_matrix_set(userBias_t, user-1, 0,
                 gsl_matrix_get(userBias_t, user-1, 0) +
-                get_mu_all_rating(point) - AVG_RATING -
+                get_mu_all_rating(point) - (double)AVG_RATING -
                 gsl_matrix_get(movieBias, movie-1, 0));
 
             gsl_matrix_set(userBias_t, user-1, 1,
                 gsl_matrix_get(userBias_t, user-1, 1) + 1);
         }
     }
-    
+    printf("\tSaving user biases...\n");
     for(int user = 0; user < USER_COUNT; user++){
         bias = ((float)gsl_matrix_get(userBias_t, user, 0)) / 
                (((float)REGUL_BIAS_USER) + gsl_matrix_get(userBias_t, user, 1));
@@ -67,23 +74,161 @@ void Baseline::calculate_average_biases(int partition){
     }
 }
 
+void Baseline::calculate_movie_time_effects(int partition){
+    int user;
+    int movie;
+    int date;
+    int binNumber;
+    float bias;
+    gsl_matrix *movieBias_t = gsl_matrix_calloc(MOVIE_COUNT, NUM_MOVIE_BINS * 2);
+    printf("\tCalculating movie bins...\n");
+    for(int point = 0; point < DATA_COUNT; point++){
+        if(get_mu_idx_ratingset(point) < partition){
+            movie = get_mu_all_movienumber(point);
+            date = get_mu_all_datenumber(point);
+            binNumber = (date - 1) / MOVIE_BIN_SIZE;
+
+            gsl_matrix_set(movieBias_t, movie-1, binNumber,
+                gsl_matrix_get(movieBias_t, movie-1, binNumber) +
+                get_mu_all_rating(point) - (double)AVG_RATING -
+                gsl_matrix_get(movieBias, movie-1, binNumber));
+
+            gsl_matrix_set(movieBias_t, movie-1, NUM_MOVIE_BINS+binNumber,
+                gsl_matrix_get(movieBias_t, movie-1, NUM_MOVIE_BINS+binNumber) + 1);
+        }
+    }
+    printf("\tSaving movie bins...\n");
+    int numRatings;
+    for(int movie = 0; movie < MOVIE_COUNT; movie++){
+        for(int bin = 0; bin < NUM_MOVIE_BINS; bin++){
+            numRatings = ((float)gsl_matrix_get(movieBias_t, movie, NUM_MOVIE_BINS + bin));
+            if(numRatings != 0){
+                bias = ((float)gsl_matrix_get(movieBias_t, movie, bin)) /
+                       ((float)numRatings);
+                gsl_matrix_set(movieBias, movie, bin, gsl_matrix_get (movieBias, movie, bin) + bias);
+            }
+        }
+    }
+}
+
+void Baseline::calculate_user_time_effects(int partition){
+    int user;
+    int movie;
+    int binNumber;
+    double date;
+    double avgdate;
+    double dateFactor;
+    double bias;
+    printf("\tCalculating average rating dates...\n");
+    //for user, matrix will have (avgdate, covxy, varx)
+    gsl_matrix *userBias_t = gsl_matrix_calloc(USER_COUNT, 3);
+    for(int point = 0; point < DATA_COUNT; point++){
+        if(get_mu_idx_ratingset(point) < partition){
+            user = get_mu_all_usernumber(point);
+            date = (double)get_mu_all_datenumber(point)/(double)DATE_COUNT;
+
+            dateFactor = pow((double)date, USER_DATE_EXP);
+
+            gsl_matrix_set(userBias_t, user-1, 0,
+                gsl_matrix_get(userBias_t, user-1, 0) +
+                date);
+
+            gsl_matrix_set(userBias_t, user-1, 1,
+                gsl_matrix_get(userBias_t, user-1, 1) +
+                date);
+        }
+    }
+    //Find average over dates
+    for(user = 0; user < USER_COUNT; user++){
+        avgdate = ((double)gsl_matrix_get(userBias_t, user, 0))/
+               ((double)gsl_matrix_get(userBias_t, user, 1));
+        gsl_matrix_set(userBias_t, user, 0, date);
+        gsl_matrix_set(userBias_t, user, 1, 0.0);
+    }
+    printf("\tCalculating regression for ratings vs dates...\n");
+    double covxy;
+    double varx;
+    for(int point = 0; point < DATA_COUNT; point++){
+        if(get_mu_idx_ratingset(point) < partition){
+            user = get_mu_all_usernumber(point);
+            movie = get_mu_all_movienumber(point);
+            date = (double)get_mu_all_datenumber(point)/(double)DATE_COUNT;
+            binNumber = (date - 1) / MOVIE_BIN_SIZE;
+            avgdate = gsl_matrix_get(userBias_t, user-1, 0);
+        
+            dateFactor = pow((double)date, USER_DATE_EXP);
+            
+            covxy = gsl_matrix_get(userBias_t, user-1, 1) +
+                (get_mu_all_rating(point) - (double)AVG_RATING -
+                 gsl_matrix_get(movieBias, movie-1, binNumber) -
+                 gsl_matrix_get(userBias, user-1, 0)) *
+                (dateFactor - avgdate);
+            
+
+            varx = gsl_matrix_get(userBias_t, user-1, 2) + pow(dateFactor - avgdate, 2.0);
+            gsl_matrix_set(userBias_t, user-1, 1, covxy);
+
+            gsl_matrix_set(userBias_t, user-1, 2, varx);
+        }
+    }
+    printf("\tSaving regression for ratings vs dates...\n");
+    double slope;
+    for(user = 0; user < USER_COUNT; user++){
+        covxy = ((double)gsl_matrix_get(userBias_t, user, 1));
+        varx = ((double)gsl_matrix_get(userBias_t, user, 2));
+        if (varx != 0){
+            slope = covxy/(varx + (double)USER_DATE_REGUL);
+        }else{
+            slope = 0.0;
+        }        
+        gsl_matrix_set(userBias_t, user, 1, slope);
+        gsl_matrix_set(userBias_t, user, 2, 0.0);
+    
+    }
+
+    double intercept;
+    double avgrating;
+    for(user = 0; user < USER_COUNT; user++){
+        avgdate = gsl_matrix_get(userBias_t, user, 0);
+        dateFactor = pow(avgdate, USER_DATE_EXP);
+        slope = gsl_matrix_get(userBias_t, user, 1);
+        if(slope != 0.0){
+            avgrating = gsl_matrix_get(userBias, user, 0);
+            intercept = avgrating - slope * dateFactor;
+        
+            gsl_matrix_set(userBias, user, 0, intercept);
+        }
+        gsl_matrix_set(userBias, user, 1, slope);
+    }
+}
+
 double Baseline::predict(int user, int movie, int time){
-    double rating = AVG_RATING + gsl_matrix_get(userBias, user-1,0) +
-                                 gsl_matrix_get(movieBias, movie-1,0);
+    int movieBin = (time - 1) / MOVIE_BIN_SIZE;
+    double movieFactor = gsl_matrix_get(movieBias, movie-1, movieBin);
+    double date = (double)time/(double)DATE_COUNT;
+    double userFactor = gsl_matrix_get(userBias, user-1, 0) +
+                        gsl_matrix_get(userBias, user-1, 1) *
+                        pow(date, USER_DATE_EXP);
+    double rating = AVG_RATING + userFactor + movieFactor;
     return rating;
 }
 
 void Baseline::remember(int partition){
     FILE *outFile;
     outFile = fopen(BASELINE_FILE, "w");
-    float bias;
+    float bias, intercept, slope;
     for(int user = 0; user < USER_COUNT; user++){
-        bias = gsl_matrix_get(userBias, user, 0);
-        fprintf(outFile,"%f\n",bias);
+        intercept = gsl_matrix_get(userBias, user, 0);
+        slope = gsl_matrix_get(userBias, user, 1);
+        fprintf(outFile,"%f\t",intercept);
+        fprintf(outFile,"%f\n",slope);
     }
     for(int movie = 0; movie < MOVIE_COUNT; movie++){
-        bias = gsl_matrix_get(movieBias, movie, 0);	
-        fprintf(outFile,"%f\n",bias);
+        for(int bin = 0; bin < NUM_MOVIE_BINS; bin++){
+            bias = gsl_matrix_get(movieBias, movie, bin);	
+            fprintf(outFile,"%f\t",bias);
+        }
+        fprintf(outFile,"\n");
     }
     fclose(outFile);
     return;
@@ -96,7 +241,8 @@ float Baseline::rmse_probe(){
     for(int i = 0; i < DATA_COUNT; i++) {
         if(get_mu_idx_ratingset(i) == 4){
             double prediction = predict(get_mu_all_usernumber(i),
-                                                  (int)get_mu_all_movienumber(i),0);
+                                                  (int)get_mu_all_movienumber(i),
+                                                  (int)get_mu_all_datenumber(i));
             double error = (prediction - (double)get_mu_all_rating(i));
             RMSE = RMSE + (error * error);
             count++;
@@ -111,7 +257,7 @@ void Baseline::load_data(){
     assert(load_mu_all_movienumber() == 0);
     assert(load_mu_all_rating() == 0);
     assert(load_mu_idx_ratingset() == 0);
-
+    assert(load_mu_all_datenumber() == 0);
     data_loaded = true;
 }
 
--- Baseline/baseline_predictor.h
+++ Baseline/baseline_predictor.h
@@ -5,10 +5,18 @@
 #define USER_COUNT 458293
 #define MOVIE_COUNT 17770
 #define DATA_COUNT 102416306
+#define DATE_COUNT 2243
 #define AVG_RATING 3.6095 //Computed over the first 3 partitions
 
+//Global averages
 #define REGUL_BIAS_MOVIE 25
 #define REGUL_BIAS_USER 10
+
+//Time effects
+#define MOVIE_BIN_SIZE 90
+#define NUM_MOVIE_BINS 25
+#define USER_DATE_EXP 4 //5.1
+#define USER_DATE_REGUL 2
 
 #define BASELINE_FILE "mu_baseline.dta"
 
@@ -21,6 +29,8 @@ class Baseline: public IPredictor{
         gsl_matrix *movieBias;
         void load_data();
         void calculate_average_biases(int partition);
+        void calculate_movie_time_effects(int partition);
+        void calculate_user_time_effects(int partition);
     public:
         Baseline();
         float rmse_probe();
