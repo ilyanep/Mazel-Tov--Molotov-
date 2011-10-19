c196ea56c606f72956228e12d40b3fa6f552c122
Ilya Nepomnyashchiy <ilyanep@gmail.com>
2011-10-19 16:10:23 -0700

    Freeing matrices. Hopefully the blender will now run within memory.

--- Blending/blender.cpp
+++ Blending/blender.cpp
@@ -66,6 +66,7 @@ void LinearBlender::learn(int partition) {
 
 gsl_vector * LinearBlender::aggregator_solution(gsl_matrix* predictions_matrix, gsl_vector* ratings_vector, 
                                                 int64 pred_points, int64 pred_num) {
+    cout << "Calculating aggregator weights" << endl;
     // Solution is (G^T G)^-1 G^T r where G is predictions_matrix and r is ratings_vector
     gsl_matrix * gtrans_g = gsl_matrix_alloc(pred_num, pred_num);
     gsl_matrix * gtrans_g_inverse = gsl_matrix_alloc(pred_num, pred_num);
@@ -74,27 +75,35 @@ gsl_vector * LinearBlender::aggregator_solution(gsl_matrix* predictions_matrix, 
     gsl_matrix * gtrans_g_inverse_gtrans = gsl_matrix_alloc(pred_num, pred_points);
     gsl_vector * final_vector = gsl_vector_alloc(pred_points);
 
+    cout << "G^T G" << endl;
     // Calculate G^T G
     gsl_matrix_set_zero(gtrans_g);
     gsl_blas_dgemm(CblasTrans, CblasNoTrans, 1.0, predictions_matrix, predictions_matrix,
                    1.0, gtrans_g);
 
+    cout << "(G^T G)^{-1}" << endl;
     // Inverse of G^T G
     gsl_permutation * p = gsl_permutation_alloc(pred_points);
     int *signum;
     *signum = 1; // Does anyone know what the hell this is?
     gsl_linalg_LU_decomp(gtrans_g, p, signum);
     gsl_linalg_LU_invert(gtrans_g, p, gtrans_g_inverse);
+    gsl_matrix_free(gtrans_g);
 
+    cout << "G^T" << endl;
     // Get G^T
     gsl_matrix_set_zero(gtrans);
     gsl_matrix_set_identity(identity);
     gsl_blas_dgemm(CblasTrans, CblasNoTrans, 1.0, predictions_matrix, identity, 1.0, gtrans);
 
+    cout << "(G^T G)^-1 G^T" << endl;
    // Multiply (G^T G)^-1 G^T
    gsl_matrix_set_zero(gtrans_g_inverse_gtrans);
    gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, gtrans_g_inverse, gtrans, 1.0, gtrans_g_inverse_gtrans);
+   gsl_matrix_free(gtrans_g_inverse);
+   gsl_matrix_free(gtrans);
 
+   cout << "Vector multiplication" << endl;
    // Do the vector multpilication 
    gsl_vector_set_zero(final_vector);
    gsl_blas_dgemv(CblasNoTrans, 1.0, gtrans_g_inverse_gtrans, ratings_vector, 1.0, final_vector);
