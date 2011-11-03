28b81b57116ac0e585f819f026f9761e33fe5676
Daniil Kitchaev <dkitch@caltech.edu>
2011-11-03 08:33:29 -0700

    movie_knn update.

--- Blending/run_blender.cpp
+++ Blending/run_blender.cpp
@@ -10,6 +10,7 @@ using namespace std;
 #include "../SVD_Oct18/svd_oct18.h"
 #include "../SVD_Oct25/svd_oct25.h"
 #include "../SVD/learn_svd.h"
+#include "../movie_knn/movie_knn.h"
 #include "../Baseline_Oct25/baseline_oct25.h"
 
 #define BLENDER_LEARNING_PARTITION 4
@@ -31,10 +32,14 @@ int main() {
     Oct25_SVD svd_oct25;
 	Oct25_Baseline baseline_oct25;
     SVD svd_nov3;
+    //Movie_Knn mknn;
+    Movie_Knn_Pearson mknn_pearson;
     predictor_vector.push_back(&svd_oct18);
     predictor_vector.push_back(&svd_oct25);
     predictor_vector.push_back(&baseline_oct25);
     predictor_vector.push_back(&svd_nov3);
+    //predictor_vector.push_back(&mknn);
+    predictor_vector.push_back(&mknn_pearson);
 
     // Initialize your mom
     cout << "Loarning linear blender" << endl;
