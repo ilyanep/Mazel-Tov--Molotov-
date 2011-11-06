c098eb2bf3fc86a808220562de0152a523f99899
Daniil Kitchaev <dkitch@caltech.edu>
2011-11-06 00:01:59 -0700

    Made the code cleaner.

--- SVD_Oct25/svd_oct25.h
+++ SVD_Oct25/svd_oct25.h
@@ -3,23 +3,9 @@
 #include "../Baseline_Oct25/baseline_oct25.h"
 #include <string>
 
-#define OCT25_SVD_DIM 16 //Seems that only first 14 actually matter
-#define OCT25_USER_COUNT 458293
-#define OCT25_MOVIE_COUNT 17770
-#define OCT25_DATA_COUNT 102416306
-#define OCT25_LEARN_EPOCHS_MIN 60 //Papers suggest a big dip in RMSE if >50?
-#define OCT25_REFINE_EPOCHS_MIN 2 //Feasible time-wise
-#define OCT25_MIN_RMSE_IMPROVEMENT 0.0001 //Arbitrary
-#define OCT25_LEARN_RATE 0.001 //Suggested optimal by a lot of papers
-#define OCT25_REGUL_PARAM 0.015 //Suggested optimal by a lot of papers
-#define OCT25_AVG_RATING 3.6095 //Computed over the first 3 partitions
-#define OCT25_INIT_SVD_VAL 0.1 //Suggested optimal by a lot of papers
+#define SVD_OCT25_PARAM_FILE "../SVD_Oct25/svd_params.dta"
 
-#define OCT25_SVD_PARAM_FILE "../SVD_Oct25/svd_params.dta"
-
-#define OCT25_SUBMIT_NUM_POINTS 2749898
-
-class Oct25_SVD: public IPredictor{
+class SVD_Oct25: public IPredictor{
     private:
         Oct25_Baseline base_predict;
         double learn_rate;
@@ -32,7 +18,19 @@ class Oct25_SVD: public IPredictor{
         double predict_point_train(int user, int movie, int time, int svd_pt);
         void load_data();
     public:
-        Oct25_SVD();
+        static const int SVD_DIM = 16; //Seems that only first 14 actually matter
+        static const int USER_COUNT = 458293;
+        static const int MOVIE_COUNT = 17770;
+        static const int DATA_COUNT = 102416306;
+        static const int LEARN_EPOCHS_MIN = 60; //Papers suggest a big dip in RMSE if >50?
+        static const int REFINE_EPOCHS_MIN = 2; //Feasible time-wise
+        static const double MIN_RMSE_IMPROVEMENT = 0.0001; //Arbitrary
+        static const double LEARN_RATE = 0.001; //Suggested optimal by a lot of papers
+        static const double REGUL_PARAM = 0.015; //Suggested optimal by a lot of papers
+        static const double AVG_RATING = 3.6095; //Computed over the first 3 partitions
+        static const double INIT_SVD_VAL = 0.1; //Suggested optimal by a lot of papers
+        static const int SUBMIT_NUM_POINTS = 2749898;
+        SVD_Oct25();
         void save_svd(int partition);
         double rmse_probe();
         void learn(int partition, bool refine);
