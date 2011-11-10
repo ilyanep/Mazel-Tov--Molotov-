be0e4c3243cc9200e7a44c6f19203ad45593e1a9
Daniil Kitchaev <dkitch@caltech.edu>
2011-11-09 22:48:55 -0800

    Cleaned up the code.

--- SVD_Oct18/generate_bias_file.cpp
+++ SVD_Oct18/generate_bias_file.cpp
@@ -14,11 +14,11 @@ int main(int argc, char* argv[]) {
     assert(load_mu_idx_ratingset() == 0);
     
     printf("Calculating user/movie biases...\n");
-    gsl_matrix *userBias = gsl_matrix_calloc(OCT18_USER_COUNT, 2);
-    gsl_matrix *movieBias = gsl_matrix_calloc(OCT18_MOVIE_COUNT, 2);
+    gsl_matrix *userBias = gsl_matrix_calloc(SVD_Oct18::USER_COUNT, 2);
+    gsl_matrix *movieBias = gsl_matrix_calloc(SVD_Oct18::MOVIE_COUNT, 2);
     int user;
     int movie;
-    for(int point = 0; point < OCT18_DATA_COUNT; point++){
+    for(int point = 0; point < SVD_Oct18::DATA_COUNT; point++){
         if(get_mu_idx_ratingset(point) < 5){
             user = get_mu_all_usernumber(point);
             movie = get_mu_all_movienumber(point);
@@ -42,14 +42,14 @@ int main(int argc, char* argv[]) {
     FILE *outFile;
     outFile = fopen(OCT18_SVD_BIAS_FILE, "w");
     float bias;
-    for(int user = 0; user < OCT18_USER_COUNT; user++){
-        bias = (((float)OCT18_AVG_RATING * OCT18_REGUL_BIAS_PARAM) + gsl_matrix_get(userBias, user, 0)) / 
-               (((float)OCT18_REGUL_BIAS_PARAM) + gsl_matrix_get(userBias, user, 1)) - OCT18_AVG_RATING;
+    for(int user = 0; user < SVD_Oct18::USER_COUNT; user++){
+        bias = (((float)SVD_Oct18::AVG_RATING * SVD_Oct18::REGUL_BIAS_PARAM) + gsl_matrix_get(userBias, user, 0)) / 
+               (((float)SVD_Oct18::REGUL_BIAS_PARAM) + gsl_matrix_get(userBias, user, 1)) - SVD_Oct18::AVG_RATING;
         fprintf(outFile,"%f\n",bias);
     }
-    for(int movie = 0; movie < OCT18_MOVIE_COUNT; movie++){
-        bias = (((float)OCT18_AVG_RATING * OCT18_REGUL_BIAS_PARAM) + gsl_matrix_get(movieBias, movie, 0)) /
-               (((float)OCT18_REGUL_BIAS_PARAM) + gsl_matrix_get(movieBias, movie, 1)) - OCT18_AVG_RATING;
+    for(int movie = 0; movie < SVD_Oct18::MOVIE_COUNT; movie++){
+        bias = (((float)SVD_Oct18::AVG_RATING * SVD_Oct18::REGUL_BIAS_PARAM) + gsl_matrix_get(movieBias, movie, 0)) /
+               (((float)SVD_Oct18::REGUL_BIAS_PARAM) + gsl_matrix_get(movieBias, movie, 1)) - SVD_Oct18::AVG_RATING;
         fprintf(outFile,"%f\n",bias);
     }
     printf("Done!");
--- SVD_Oct18/svd_oct18.h
+++ SVD_Oct18/svd_oct18.h
@@ -1,29 +1,15 @@
-#ifndef SVD_SVD_OCT18_H
-#define SVD_SVD_OCT18_H
+#ifndef SVD_OCT18_H
+#define SVD_OCT18_H
 
 #include <gsl/gsl_matrix.h>
 #include "../learning_method.h"
 #include <string>
 
-#define OCT18_SVD_DIM 64 //Seems that only first 14 actually matter
-#define OCT18_USER_COUNT 458293
-#define OCT18_MOVIE_COUNT 17770
-#define OCT18_DATA_COUNT 102416306
-#define OCT18_LEARN_EPOCHS_MIN 60 //Papers suggest a big dip in RMSE if >50?
-#define OCT18_REFINE_EPOCHS_MIN 2 //Feasible time-wise
-#define OCT18_MIN_RMSE_IMPROVEMENT 0.0001 //Arbitrary
-#define OCT18_LEARN_RATE 0.001 //Suggested optimal by a lot of papers
-#define OCT18_REGUL_PARAM 0.015 //Suggested optimal by a lot of papers
-#define OCT18_REGUL_BIAS_PARAM 25 //Suggested optimal by a lot of papers
-#define OCT18_AVG_RATING 3.6095 //Computed over the first 3 partitions
-#define OCT18_INIT_SVD_VAL 0.1 //Suggested optimal by a lot of papers
 
 #define OCT18_SVD_PARAM_FILE "../SVD_Oct18/svd_params.dta"
 #define OCT18_SVD_BIAS_FILE "../SVD_Oct18/svd_bias.dta"
 
-#define OCT18_SUBMIT_NUM_POINTS 2749898
-
-class Oct18_SVD: public IPredictor{
+class SVD_Oct18: public IPredictor{
     private:
         float learn_rate;
         float svd_regul;
@@ -35,7 +21,21 @@ class Oct18_SVD: public IPredictor{
         float predict_point_train(int user, int movie, int svd_pt);
         void load_data();
     public:
-        Oct18_SVD();
+        static const int SVD_DIM = 64; //Seems that only first 14 actually matter
+        static const int USER_COUNT = 458293;
+        static const int MOVIE_COUNT = 17770;
+        static const int DATA_COUNT = 102416306;
+        static const int LEARN_EPOCHS_MIN = 60; //Papers suggest a big dip in RMSE if >50?
+        static const int REFINE_EPOCHS_MIN = 2; //Feasible time-wise
+        static const double MIN_RMSE_IMPROVEMENT = 0.0001; //Arbitrary
+        static const double LEARN_RATE = 0.001; //Suggested optimal by a lot of papers
+        static const double REGUL_PARAM = 0.015; //Suggested optimal by a lot of papers
+        static const double REGUL_BIAS_PARAM = 25; //Suggested optimal by a lot of papers
+        static const double AVG_RATING = 3.6095; //Computed over the first 3 partitions
+        static const double INIT_SVD_VAL = 0.1; //Suggested optimal by a lot of papers
+        static const int SUBMIT_NUM_POINTS = 2749898;
+
+        SVD_Oct18();
         void save_svd(int partition);
         float rmse_probe();
         void learn(int partition, bool refine);
