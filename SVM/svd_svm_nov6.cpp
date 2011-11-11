7f2770f58338ee0291072e4ca6fc3b22a103204e
Daniil Kitchaev <dkitch@caltech.edu>
2011-11-10 20:51:46 -0800

    Full SVM predictor written.

--- SVM/svd_svm_nov6.cpp
+++ SVM/svd_svm_nov6.cpp
@@ -11,191 +11,47 @@ using namespace std;
 #include <iostream>
 #include <math.h>
 #include "svd_svm_nov6.h"
+#include "libsvm-3.1/svm.h"
 
+#define Malloc(type,n) (type *)malloc((n)*sizeof(type))
 
 SVD_SVM_Nov6::SVD_SVM_Nov6(){
-    //Initially all matrix elements are set to 0.1
-    userSVD = gsl_matrix_calloc(USER_COUNT, SVD_DIM);
-    movieSVD = gsl_matrix_calloc(SVD_DIM, MOVIE_COUNT);
-    for(int i = 0; i < USER_COUNT; i++){
-        for(int p = 0; p < SVD_DIM; p++){
-            gsl_matrix_set(userSVD, i, p, INIT_SVD_VAL);
-        }
-    }
-	for(int i = 0; i < MOVIE_COUNT; i++){
-        for(int p = 0; p < SVD_DIM; p++){
-            gsl_matrix_set(movieSVD, p, i, INIT_SVD_VAL);
-        }
-    }
     data_loaded = false;
-    learn_rate = LEARN_RATE;
-    svd_regul = REGUL_PARAM;
+    svd_loaded = false;
     load_data();
-    base_predict = Oct25_Baseline(data_loaded);
-    srand(time(NULL));
+
+    base_predict = Baseline_Oct25(data_loaded);
+    base_predict.remember(BASE_PARTITION);
 }
 
 void SVD_SVM_Nov6::learn(int partition){
-    learn(partition, false);
+    load_svd_parameters();
+
+    generate_svm_input(partition);
+    generate_svm_param();
+    free_svd();
+
+    model = svm_train(&prob,&param);
+
+    svm_destroy_param(&param);
+	free(trainData.y);
+	free(trainData.x);
+	free(x_space);
 }
 
-void SVD_SVM_Nov6::learn(int partition, bool refining){
-    assert(data_loaded);
-
-    /* Load bias parameters */
-    printf("Loading baseline...\n");
-    base_predict.remember(partition);
-
-    printf("Computing SVD...\n");
-    int param_start = 0;
-    int min_epochs = LEARN_EPOCHS_MIN;
-    double init_value = INIT_SVD_VAL;
-    if(refining){
-        int svd_pt = 0;
-        while(svd_pt < SVD_DIM && gsl_matrix_get(userSVD, 0, svd_pt) != init_value){
-            svd_pt++;
-        }
-        if(svd_pt != SVD_DIM)
-            param_start = svd_pt;
-        min_epochs = REFINE_EPOCHS_MIN;
-    }
-    /* Cycle through dataset */
-    for(int p = param_start; p < SVD_DIM; p++){
-        printf("\nSVD Parameter %u: Learning... %u percent.\n", p+1, (int)((double)(p)*100.0/(double)SVD_DIM));
-        int k = 0;
-        int point_count;
-        double err;
-        double errsq;
-        double rmse = 10.0;
-        double oldrmse = 100.0;
-        while(fabs(oldrmse - rmse) > MIN_RMSE_IMPROVEMENT || k < min_epochs){
-        //while(oldrmse - rmse > MIN_RMSE_IMPROVEMENT){
-            oldrmse = rmse;
-            k++;
-            errsq = 0.0;
-            point_count = 0;
-            for(int i = 0; i < DATA_COUNT; i++){
-                if(get_mu_idx_ratingset(i) <= partition){
-                    err = learn_point(p, get_mu_all_usernumber(i)-1,
-                                         get_mu_all_movienumber(i)-1,
-                                         get_mu_all_datenumber(i),
-                                         get_mu_all_rating(i), refining);
-                    if(err != -999){
-                        errsq = errsq + err * err;
-                        point_count++;
-                    }
-                }
-            }
-            rmse = errsq / ((double)point_count);
-            printf("\tEpoch %u: RMSE(in): %f; RMSE(out): %f\n", k, sqrt(rmse),rmse_probe());
-        }
-        save_svd(3);
-    }
+void SVD_SVM_Nov6::free_svd(){
+    if(!svd_loaded)
+        return;
+    gsl_matrix_free(userSVD);
+    gsl_matrix_free(movieSVD);
+    svd_loaded = false;
 }
 
-double SVD_SVM_Nov6::learn_point(int svd_pt, int user, int movie, int time, double rating, bool refining){
-    if(rating == 0)
-        return -999;
-    double err;
-    if(refining)
-        err = rating - predict_point(user, movie, time);
-    else
-	    err = rating - predict_point_train(user, movie, time, svd_pt);
+void SVD_SVM_Nov6::load_svd_parameters(){
+    userSVD = gsl_matrix_calloc(USER_COUNT, SVD_DIM);
+    movieSVD = gsl_matrix_calloc(SVD_DIM, MOVIE_COUNT);
+    assert(userSVD != NULL && movieSVD != NULL);
 
-    double svd_user_old = gsl_matrix_get(userSVD, user, svd_pt); 
-    double svd_movie_old = gsl_matrix_get(movieSVD, svd_pt, movie); 
-	gsl_matrix_set(userSVD, user, svd_pt, svd_user_old + 
-                  (learn_rate * (err * svd_movie_old -
-                  svd_regul * svd_user_old)));
-	gsl_matrix_set(movieSVD, svd_pt, movie, svd_movie_old +
-                  (learn_rate * (err * svd_user_old -
-                  svd_regul * svd_movie_old)));
-    return err;
-}
-
-void SVD_SVM_Nov6::save_svd(int partition){
-    FILE *outFile;
-    outFile = fopen(SVD_OCT25_PARAM_FILE, "w");
-    fprintf(outFile,"%u\n",partition);
-    for(int user = 0; user < USER_COUNT; user++){
-        for(int i = 0; i < SVD_DIM; i++) {
-            fprintf(outFile,"%lf ",gsl_matrix_get(userSVD, user, i)); 
-        }
-        fprintf(outFile,"\n");
-    }
-    for(int movie = 0; movie < MOVIE_COUNT; movie++){
-        for(int i = 0; i < SVD_DIM; i++) {
-            fprintf(outFile,"%lf ",gsl_matrix_get(movieSVD, i, movie));
-        }
-    }
-    fclose(outFile);
-    return;
-}
-
-void SVD_SVM_Nov6::output_svm_data_files(int train_partition){
-    FILE *trainFile;
-    trainFile = fopen(SVM_TRAIN_FILE, "w");
-
-    printf("Outputting training file...\n");
-    for(int point = 0; point < DATA_COUNT; point++){
-        if( point % 10000000 == 0)
-            printf("\tOutputting... %i percent\n", (int)((float)(point*100.0/DATA_COUNT)) );
-        if(get_mu_idx_ratingset(point) == train_partition){
-            fprintf(trainFile,"%i",get_mu_all_rating(point));
-            fprintf(trainFile,"\t%d:%lf", 1, predict(get_mu_all_usernumber(point),
-                                                             get_mu_all_movienumber(point),
-                                                             get_mu_all_datenumber(point)) );
-            for(int i = 0; i < SVD_DIM; i++)
-                fprintf(trainFile,"\t%d:%lf", i+2, gsl_matrix_get(userSVD, get_mu_all_usernumber(point)-1, i));
-            for(int i = 0; i < SVD_DIM; i++)
-                fprintf(trainFile,"\t%d:%lf", i+2+SVD_DIM, gsl_matrix_get(movieSVD, i, get_mu_all_movienumber(point)-1));
-            fprintf(trainFile,"\n");
-        }
-    }
-    fclose(trainFile);
-    printf("Outputting testing file...\n");
-
-    /*FILE *testFile;
-    testFile = fopen(SVM_TEST_FILE, "w");
-    for(int point = 0; point < DATA_COUNT; point++){
-        if( point % 10000000 == 0)
-            printf("\tOutputting... %i percent\n", (int)((float)(point*100.0/DATA_COUNT)) );
-        if(get_mu_idx_ratingset(point) == 4 && point % 10000 == 0){
-            fprintf(testFile,"%i",get_mu_all_rating(point));
-            fprintf(testFile,"\t%d:%lf", 1, predict(get_mu_all_usernumber(point),
-                                                             get_mu_all_movienumber(point),
-                                                             get_mu_all_datenumber(point)) );
-            for(int i = 0; i < SVD_DIM; i++)
-                fprintf(testFile,"\t%d:%lf", i+2, gsl_matrix_get(userSVD, get_mu_all_usernumber(point)-1, i));
-            for(int i = 0; i < SVD_DIM; i++)
-                fprintf(testFile,"\t%d:%lf", i+2+SVD_DIM, gsl_matrix_get(movieSVD, i, get_mu_all_movienumber(point)-1));
-            fprintf(testFile,"\n");
-        }
-    }
-    fclose(testFile);*/
-
-    FILE *outputFile;
-    outputFile = fopen(SVM_OUTPUT_FILE, "w");
-    for(int point = 0; point < DATA_COUNT; point++){
-        if( point % 10000000 == 0)
-            printf("\tOutputting... %i percent\n", (int)((float)(point*100.0/DATA_COUNT)) );
-        if(get_mu_idx_ratingset(point) == 5){
-            fprintf(outputFile,"%i",get_mu_all_rating(point));
-            fprintf(outputFile,"\t%d:%lf", 1, predict(get_mu_all_usernumber(point),
-                                                             get_mu_all_movienumber(point),
-                                                             get_mu_all_datenumber(point)) );
-            for(int i = 0; i < SVD_DIM; i++)
-                fprintf(outputFile,"\t%d:%lf", i+2, gsl_matrix_get(userSVD, get_mu_all_usernumber(point)-1, i));
-            for(int i = 0; i < SVD_DIM; i++)
-                fprintf(outputFile,"\t%d:%lf", i+2+SVD_DIM, gsl_matrix_get(movieSVD, i, get_mu_all_movienumber(point)-1));
-            fprintf(outputFile,"\n");
-        }
-    }
-    fclose(outputFile);
-}
-
-void SVD_SVM_Nov6::remember(int partition){
-    base_predict.remember(partition);
     FILE *inFile;
     inFile = fopen(SVD_OCT25_PARAM_FILE, "r");
     assert(inFile != NULL);
@@ -203,7 +59,7 @@ void SVD_SVM_Nov6::remember(int partition){
     //printf("File opened.\n");
     double g = 0.0;
     fscanf(inFile,"%u",&load_partition);
-    assert(load_partition == partition);
+    assert(load_partition == SVD_PARTITION);
     for(int user = 0; user < USER_COUNT; user++){
         for(int i = 0; i < SVD_DIM; i++) {
             fscanf(inFile, "%lf", &g);
@@ -217,8 +73,90 @@ void SVD_SVM_Nov6::remember(int partition){
         }
     }
     fclose(inFile);
+    svd_loaded = true;
     return;
+}
 
+void SVD_SVM_Nov6::generate_svm_input(int partition){
+    int partition_size;
+    switch(partition){
+        case 1:
+            partition_size = PART1_SIZE;
+            break;
+        case 2:
+            partition_size = PART2_SIZE;
+            break;
+        case 3:
+            partition_size = PART3_SIZE;
+            break;
+        case 4:
+            partition_size = PART4_SIZE;
+            break;
+        default:
+            printf("Incorrect partition given to svm learn. Cannot learn");
+            return;
+    }
+    trainData.l = partition_size;
+    trainData.y = Malloc(double, partition_size);
+    trainData.x = Malloc(struct svm_node *, partition_size);
+    x_space = Malloc(struct svm_node, partition_size * (SVD_DIM*2 + 2));
+    assert(trainData.y != NULL && trainData.x != NULL && x_space != NULL);
+
+    int partPoint = 0;
+    int nodePoint = 0;
+    for(int point = 0; point < DATA_COUNT; point++){
+        if( point % 10000000 == 0)
+            printf("\tGenerating... %i percent\n", (int)((float)(point*100.0/DATA_COUNT)) );
+        if(get_mu_idx_ratingset(point) == partition){
+            prob.x[partPoint] = &x_space[nodePoint];
+            prob.y[partPoint] = get_mu_all_rating(point);
+            x_space[nodePoint].index = 1;
+            x_space[nodePoint].value = svd_predict(get_mu_all_usernumber(point),
+                                                   get_mu_all_movienumber(point),
+                                                   get_mu_all_datenumber(point))
+            nodePoint++;
+            for(int i = 0; i < SVD_DIM; i++){
+                x_space[nodePoint].index = i+2;
+                x_space[nodePoint].value = gsl_matrix_get(userSVD, get_mu_all_usernumber(point)-1, i);
+                nodePoint++;
+            }for(int i = 0; i < SVD_DIM; i++)
+                x_space[nodePoint].index = i+2+SVD_DIM;
+                x_space[nodePoint].value = gsl_matrix_get(movieSVD, i, get_mu_all_movienumber(point)-1);
+                nodePoint++;
+            x_space[nodePoint].index = -1;
+            x_space[nodePoint].value = 0.0;
+            nodePoint++;
+            partPoint++;
+        }
+    }
+}
+
+void SVD_SVM_Nov6::generate_svm_param(){
+	param.svm_type = C_SVC;
+	param.kernel_type = RBF;
+	param.degree = 3;
+	param.gamma = 0.001;
+	param.coef0 = 0;
+	param.nu = 0.5;
+	param.cache_size = 6000;
+	param.C = 1;
+	param.eps = 0.9;
+	param.p = 0.1;
+	param.shrinking = 1;
+	param.probability = 0;
+	param.nr_weight = 0;
+	param.weight_label = NULL;
+	param.weight = NULL;
+	cross_validation = 0;
+}
+
+void SVD_SVM_Nov6::save_svm();
+    svm_save_model(SVM_OUTPUT_FILE, model)
+    return;
+}
+
+void SVD_SVM_Nov6::remember(int partition){
+    model = svm_load_model(SVD_OUTPUT_FILE);
 }
 
 void SVD_SVM_Nov6::load_data(){
@@ -232,11 +170,26 @@ void SVD_SVM_Nov6::load_data(){
 }
 
 double SVD_SVM_Nov6::predict(int user, int movie, int time){
-    double rating = (double)predict_point(user-1, movie-1, time);
-    if(rating < 1.0)
-        return 1.0;
-    if(rating > 5.0)
-        return 5.0;
+    if(!svd_loaded)
+        load_svd_parameters();
+    struct svm_node *x;
+    x = Malloc(struct svm_node, SVD_DIM*2 + 2);
+    assert(x != NULL);
+    int nodePoint = 0;
+    x[nodePoint].index = 1;
+    x[nodePoint].value = svd_predict(user, movie, time);
+    nodePoint++;
+    for(int i = 0; i < SVD_DIM; i++){
+        x[nodePoint].index = i+2;
+        x[nodePoint].value = gsl_matrix_get(userSVD, get_mu_all_usernumber(point)-1, i);
+        nodePoint++;
+    }for(int i = 0; i < SVD_DIM; i++)
+        x[nodePoint].index = i+2+SVD_DIM;
+        x[nodePoint].value = gsl_matrix_get(movieSVD, i, get_mu_all_movienumber(point)-1);
+        nodePoint++;
+    x[nodePoint].index = -1;
+    x[nodePoint].value = 0.0;
+
     return rating;
 }
 
@@ -244,7 +197,7 @@ double SVD_SVM_Nov6::rmse_probe(){
     double RMSE = 0.0;
     int count = 0;
     for(int i = 0; i < DATA_COUNT; i++) {
-        if(get_mu_idx_ratingset(i) == 4){
+        if(get_mu_idx_ratingset(i) == 4 && i % 10000 == 0){
             double prediction = predict(get_mu_all_usernumber(i),
                                         (int)get_mu_all_movienumber(i),
                                         (int)get_mu_all_datenumber(i));
@@ -257,22 +210,17 @@ double SVD_SVM_Nov6::rmse_probe(){
     return RMSE;
 }   
 
-double SVD_SVM_Nov6::predict_point(int user, int movie, int date){
-    double rating = base_predict.predict(user+1, movie+1, date);
+double SVD_SVM_Nov6::svd_predict(int user, int movie, int date){
+    assert(svd_loaded);
+    double rating = base_predict.predict(user, movie, date);
     for (int i = 0; i < SVD_DIM; i++){
-        rating = rating + gsl_matrix_get(userSVD, user, i) * 
-                  gsl_matrix_get(movieSVD, i, movie);   
+        rating = rating + gsl_matrix_get(userSVD, user-1, i) * 
+                  gsl_matrix_get(movieSVD, i, movie-1);   
     }
-    return rating;
-}
-
-double SVD_SVM_Nov6::predict_point_train(int user, int movie, int date, int svd_pt){
-    double rating = base_predict.predict(user+1, movie+1, date);
-    for (int i = 0; i <= svd_pt; i++){
-        rating = rating + gsl_matrix_get(userSVD, user, i) * 
-                  gsl_matrix_get(movieSVD, i, movie);
-    }
-    rating = rating + INIT_SVD_VAL * INIT_SVD_VAL * (SVD_DIM - svd_pt -1);
+    if (rating < 1.0)
+        return 1.0;
+    else if(rating > 5.0)
+        return 5.0;
     return rating;
 }
             
--- SVM/svd_svm_nov6.h
+++ SVM/svd_svm_nov6.h
@@ -7,27 +7,31 @@
 #include <string>
 
 #define SVD_OCT25_PARAM_FILE "../SVM/svd_params.dta"
-#define SVM_TRAIN_FILE "../SVM/svm_train.dta"
-#define SVM_TEST_FILE "../SVM/svm_test.dta"
 #define SVM_OUTPUT_FILE "../SVM/svm_output.dta"
 
 class SVD_SVM_Nov6: public IPredictor{
     private:
-        Oct25_Baseline base_predict;
-        double learn_rate;
-        double svd_regul;
         bool data_loaded;
+        bool svd_loaded;
         gsl_matrix *userSVD;
         gsl_matrix *movieSVD;
-        double learn_point(int svd_pt, int user, int movie, int time, double rating, bool refining);
-        double predict_point(int user, int movie, int time);
-        double predict_point_train(int user, int movie, int time, int svd_pt);
+        Baseline_Oct25 base_predict;
+        struct svm_problem trainData;
+        struct svm_parameter param;
+        struct svm_model *model;
+        struct svm_node *x_space;
+        double svd_predict(int user, int movie, int time);
         void load_data();
+        void load_svd_parameters();
+        void generate_svm_input(int partition);
+        void generate_svm_param();
     public:
         static const int SVD_DIM = 16; //Seems that only first 14 actually matter
         static const int USER_COUNT = 458293;
         static const int MOVIE_COUNT = 17770;
         static const int DATA_COUNT = 102416306;
+        static const int BASE_PARTITION = 3;
+        static const int SVD_PARTITION = 3;
         static const int LEARN_EPOCHS_MIN = 60; //Papers suggest a big dip in RMSE if >50?
         static const int REFINE_EPOCHS_MIN = 2; //Feasible time-wise
         static const double MIN_RMSE_IMPROVEMENT = 0.0001; //Arbitrary
@@ -36,14 +40,14 @@ class SVD_SVM_Nov6: public IPredictor{
         static const double AVG_RATING = 3.6095; //Computed over the first 3 partitions
         static const double INIT_SVD_VAL = 0.1; //Suggested optimal by a lot of papers
         static const int SUBMIT_NUM_POINTS = 2749898;
+
         SVD_SVM_Nov6();
-        void save_svd(int partition);
+        void save_svm();
         double rmse_probe();
         void learn(int partition, bool refine);
-        void output_svm_data_files(int train_partition);
+        void free_svd();
         virtual void learn(int partition);
         virtual double predict(int user, int movie, int time);
-        //double svdpredict(int user, int movie, int time);
         virtual void remember(int partition);
         
       
