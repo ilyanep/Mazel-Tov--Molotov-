0f1f1810170240d7aa8c764829c814da90706aa8
Ilya Nepomnyashchiy <ilyanep@gmail.com>
2011-11-18 07:58:37 -0800

    Make rbm_executable always do rmse_probe

--- RBM/rbm_executable.cpp
+++ RBM/rbm_executable.cpp
@@ -6,8 +6,9 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    if(argc != 2 && argc != 1) {
-        cout << "rbm_executable [--rmse_probe]" << endl;
+    if(argc != 1) {
+        cout << "rbm_executable" << endl;
+        return 0;
     }
     time_t init;
     time_t end;
@@ -18,13 +19,7 @@ int main(int argc, char* argv[]) {
     time(&end);
     cout << "Learning took " << difftime(init, end) << " seconds" << endl;
 
-    if(argc == 2 && argv[1] == "--rmse_probe") {
-        cout << "Starting RMSE on probe calculation." << endl;
-        cout << predictor.rmse_probe() << endl;
-    }
-    else if(argc > 1) {
-        cout << "Unknown argument " << argv[1] << endl;
-        cout << "You dolt." << endl;
-    }
+    cout << "Starting RMSE on probe calculation." << endl;
+    cout << predictor.rmse_probe() << endl;
     return 0;
 }
