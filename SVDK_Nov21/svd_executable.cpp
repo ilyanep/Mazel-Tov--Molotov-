af930bb961e53e962d2db657f7c807fc98cab5f6
Daniil Kitchaev <dkitch@caltech.edu>
2011-11-21 09:55:06 -0800

    SVD++ without baseline, trained on probe.

---
+++ SVDK_Nov21/svd_executable.cpp
@@ -0,0 +1,46 @@
+#include <string.h>
+#include "svdk_nov21.h"
+#include <math.h>
+#include <assert.h>
+//#include <time.h>
+using namespace std;
+#include "../binary_files/binary_files.h"
+#include <gsl/gsl_matrix.h>
+#include "../write_data/write_results.h"
+int main(int argc, char* argv[]) {
+    if(argc != 2){
+        printf("Proper use is svd_executable [--load] [--refine] [--learn] [--output].\n");
+        exit(1);
+    }
+    clock_t init, final;
+    printf("Creating predictor and loading data...\n");
+    SVDK_Nov21 predictor;
+    //init=clock();
+    if(strcmp(argv[1], "--load") == 0){
+        printf("Loading SVD parameters...\n");
+        predictor.remember(4);
+    }else if(strcmp(argv[1], "--refine") == 0){
+        printf("Loading SVD parameters...\n");
+        predictor.remember(4);
+        printf("Refining SVD parameters...\n");
+        predictor.learn(4, true);
+    }else if(strcmp(argv[1], "--learn") == 0){
+        printf("Learning dataset...\n");
+        predictor.learn(4);
+    }else if(strcmp(argv[1], "--output") == 0){
+        printf("Loading SVD parameters...\n");
+        predictor.remember(4);
+        printf("Saving test predictions...\n");
+        vector<double> results;
+        //load qual data files (user,movie numbers) in mu order
+        assert(load_um_qual_usernumber() == 0);
+        assert(load_um_qual_movienumber() == 0);
+        for(int i=0; i < SVDK_Nov21::SUBMIT_NUM_POINTS; i++) {
+            results.push_back( predictor.predict(get_um_qual_usernumber(i),
+                                           (int)get_um_qual_movienumber(i),
+                                           (int)get_um_qual_datenumber(i)) );
+        }
+        output_results(results);
+    }      
+}
+
