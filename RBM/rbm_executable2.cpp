7e93e6fb28d20fa6b1ac1bc4390e00b6ddcd1d14
Ilya Nepomnyashchiy <ilyanep@gmail.com>
2011-11-26 02:30:01 -0800

    some updates

--- RBM/rbm_executable2.cpp
+++ RBM/rbm_executable2.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
     if(argc != 1) {
-        cout << "rbm_executable" << endl;
+        cout << "rbm_executable2" << endl;
         return 0;
     }
     time_t init;
