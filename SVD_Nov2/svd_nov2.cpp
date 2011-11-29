3abb554bdff0bec8468c82d5017ce7caa336584d
Daniil Kitchaev <dkitch@caltech.edu>
2011-11-28 16:58:38 -0800

    Changed to um.

--- SVD_Nov2/svd_nov2.cpp
+++ SVD_Nov2/svd_nov2.cpp
@@ -93,10 +93,10 @@ void SVD_Nov2::learn(int partition, bool refining){
             errsq = 0.0;
             point_count = 0;
             for(int i = 0; i < DATA_COUNT; i++){
-                if(get_mu_idx_ratingset(i) <= partition){
-                    err = learn_point(p, get_mu_all_usernumber(i)-1,
-                                         get_mu_all_movienumber(i)-1,
-                                         get_mu_all_rating(i)-AVG_RATING, refining);
+                if(get_um_idx_ratingset(i) <= partition){
+                    err = learn_point(p, get_um_all_usernumber(i)-1,
+                                         get_um_all_movienumber(i)-1,
+                                         get_um_all_rating(i)-AVG_RATING, refining);
                     if(err != -999){
                         errsq = errsq + err * err;
                         point_count++;
@@ -178,10 +178,10 @@ void SVD_Nov2::remember(int partition){
 }
 
 void SVD_Nov2::load_data(){
-    assert(load_mu_all_usernumber() == 0);
-    assert(load_mu_all_movienumber() == 0);
-    assert(load_mu_all_rating() == 0);
-    assert(load_mu_idx_ratingset() == 0);
+    assert(load_um_all_usernumber() == 0);
+    assert(load_um_all_movienumber() == 0);
+    assert(load_um_all_rating() == 0);
+    assert(load_um_idx_ratingset() == 0);
 
     data_loaded = true;
 }
@@ -195,10 +195,10 @@ float SVD_Nov2::rmse_probe(){
     double RMSE = 0.0;
     int count = 0;
     for(int i = 0; i < DATA_COUNT; i++) {
-        if(get_mu_idx_ratingset(i) == 4){
-            double prediction = predict(get_mu_all_usernumber(i),
-                                                  (int)get_mu_all_movienumber(i),0,0);
-            double error = (prediction - (double)get_mu_all_rating(i));
+        if(get_um_idx_ratingset(i) == 4){
+            double prediction = predict(get_um_all_usernumber(i),
+                                                  (int)get_um_all_movienumber(i),0,0);
+            double error = (prediction - (double)get_um_all_rating(i));
             RMSE = RMSE + (error * error);
             count++;
         }
